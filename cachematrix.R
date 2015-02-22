f22f38e7b84fa7c5d91523dc38f371c00ba3216b
kshitij Tumbde <kshitijnt@yahoo.com>
2015-02-22 13:48:27 -0800

    prog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is an expensive computation and there are benefits to 
+## catching the Matrix inversion than compute it each time. 
 
-## Write a short comment describing this function
+## The following functions together catch an inverse of a matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Assumption is that a matrix supplied is always invertible.
 
+## This function creates a special "matrix" object that can cache its inverse. 
+## It does 4 things as below
+## Set the value of the matrix.
+## Get the value of the matrix.
+## Set the value of the matrix inverse.
+## Get the value of the matrix inverse.
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+      m <- NULL
+      set <- function(y) 
+          {
+             x <<- y
+             m <<- NULL
+          }
+      get <- function() x
+      setinverse <- function(square) m <<- square
+      getinverse <- function() m
+      list(set = set, get = get,setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cacheSolve should retrieve the inverse from the cache and not recompute it.
+## However if the inverse has not been calculated previously then the CacheSolve function computes
+## the inverse using the setinverse function.
+
+cacheSolve <- function(x, ...) 
+{
+  m <- x$getinverse()
+  if(!is.null(m)) 
+    {
+        message("getting cached data")
+        return(m)
+    }
+  data <- x$get()
+  m <- square(data, ...)
+  x$setinverse(m)
+  m
 }
+
